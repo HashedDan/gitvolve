95cdf9098fc86acb210c956802a5ec24f2c437ef
HashedDan <georgedanielmangum@gmail.com>
2018-09-12 00:09:46 -0500

    basic gatsby structure

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'GitVolve',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './index.css'
+
+const IndexPage = () => (
+  <div>
+    <input placeholder="GitHub Repository"></input>
+  </div>
+)
+
+export default IndexPage
